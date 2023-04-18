234279f76906f388fdda918317e552f9b97029b8
RndmVariableQ <63947190+RndmVariableQ@users.noreply.github.com>
2023-04-18 17:51:18 +0800

    Update openai.ts

--- src/openai.ts
+++ src/openai.ts
@@ -64,18 +64,19 @@ async function dalle(username:string,prompt: string) {
 }
 
 /**
- * Speech to text
+ * Speech to text and generate a response using chatgpt
  * @param username
  * @param videoPath
  */
-async function whisper(username:string,videoPath: string): Promise<string> {
-  const file:any= fs.createReadStream(videoPath);
-  const response = await openai.createTranscription(file,"whisper-1")
-    .then((res) => res.data).catch((err) => console.log(err));
-  if (response) {
-    return response.text;
-  }else{
-    return "Speech to text failed"
+async function whisper(username: string, videoPath: string): Promise<string> {
+  const file: any = fs.createReadStream(videoPath);
+  const transcriptionResponse = await openai.createTranscription(file, "whisper-1");
+  const transcription = transcriptionResponse.data.text;
+  if (transcription) {
+    const response = await chatgpt(username, transcription);
+    return response;
+  } else {
+    return "Speech to text failed";
   }
 }
 
